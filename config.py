94b8ab3a27699b41736bbb6ae2a2bbb7d4b8000a
Sergey Kostukhin <sergey.kostukhin@gmail.com>
2022-12-27 06:43:23 +0300

    1

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "5988035036:AAGuoAz5zJuRuEPDO_b8poqgcxWxI0qxmwc"
+# https://t.me/FXquotesbot
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ extensions.py
@@ -0,0 +1,40 @@
+import requests
+from config import exchanges
+
+
+class APIException(Exception):
+    pass
+
+
+class Convertor:
+    @staticmethod
+    def get_price(base, sym, amount):
+        try:
+            base_key = exchanges[base.lower()]
+        except KeyError:
+            raise APIException(f"Валюта {base} не найдена!")
+
+        try:
+            sym_key = exchanges[sym.lower()]
+        except KeyError:
+            raise APIException(f"Валюта {sym} не найдена!")
+
+        if base_key == sym_key:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}!')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}!')
+
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={sym_key}&from={base_key}&amount={amount}"
+
+        payload = {}
+        headers = {
+            "apikey": "oYrebqhWPnCDxXh7UwUIV9dN0hOR7Wx5"
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        status_code = response.status_code
+        result = response.json()
+        return result.get('result')
